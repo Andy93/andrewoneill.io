9fd9aac2f28e83cad120700b9e9027acb2237bc0
Andy93 <oneillandrew101@gmail.com>
2018-01-29 18:58:56 +0000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,10 @@ module.exports = function(app) {
         res.render('index.ejs'); // load the index.ejs
     });
     
+    app.get('/pixel-width', function(req, res) {
+        res.render('pixel-width.ejs'); // load the pixel-width.ejs
+    });
+    
     // 404 & 500 errors
     app.use(function(req, res, next) {
       res.status(404).send('Sorry cant find that!');
