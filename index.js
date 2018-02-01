59c483e2295bf7d20f8220a8aa94b02a1f8b8e51
Andy93 <oneillandrew101@gmail.com>
2018-02-01 12:30:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,13 @@ const app = express()
 let server = require('http').Server(app);
 var port = process.env.PORT || 8000
 
-    app.get('', function(req, res){
-      res.render('index.ejs');
-    });
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
 
-    app.get('/', function(req, res){
-      res.render('index.ejs');
-    });
-
-    app.get('/playground', function(req, res){
-      res.render('playground.ejs');
-    });
-
-    app.get('/pixel-width', function(req, res){
-      res.render('pixel-width.ejs');
-    });
-
-    app.get('/mini-projects', function(req, res){
-      res.render('mini-projects.ejs');
-    });
-    
-    // 404 & 500 errors
-    app.use(function(req, res, next) {
-      res.status(404).send('Sorry cant find that!');
-    });
-
-    app.use(function(err, req, res, next) {
-      console.error(err.stack);
-      res.status(500).send('Something broke!');
-    });
+app.get('/pixel-width', function(req, res){
+  res.render('pixel-width.ejs');
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
