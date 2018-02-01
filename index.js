6c60c520843d32e7ade6b9ec19b6a476f2dd3700
Andy93 <oneillandrew101@gmail.com>
2018-02-01 12:21:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static('views'));
 
+
+    app.get('/', function(req, res){
+      res.render('index.ejs');
+    });
+
+    app.get('/playground', function(req, res){
+      res.render('playground.ejs');
+    });
+
+    app.get('/pixel-width', function(req, res){
+      res.render('pixel-width.ejs');
+    });
+
+    app.get('/mini-projects', function(req, res){
+      res.render('mini-projects.ejs');
+    });
+    
+    // 404 & 500 errors
+    app.use(function(req, res, next) {
+      res.status(404).send('Sorry cant find that!');
+    });
+
+    app.use(function(err, req, res, next) {
+      console.error(err.stack);
+      res.status(500).send('Something broke!');
+    });
+
 require('./routes/routes.js');
 
 server.listen(port, function() {
